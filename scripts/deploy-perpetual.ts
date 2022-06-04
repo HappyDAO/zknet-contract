1a54a9817a3e4cabf588d729fe8bc2f2c7e71f87
zunkk <shang123456q@gmail.com>
2022-06-04 16:36:09 +0800

    feat: adjust order structure

--- scripts/deploy-perpetual.ts
+++ scripts/deploy-perpetual.ts
@@ -1,7 +1,7 @@
 import * as zksync from "zksync-web3";
 
 import { logger } from "../util/log";
-import { PerpetualRuntime } from "../util/perpetual";
+import { ERC20_ADDRESS, PerpetualRuntime } from "../util/perpetual";
 
 async function main() {
   const runtime = new PerpetualRuntime();
@@ -12,8 +12,11 @@ async function main() {
     runtime.providerL1,
   );
 
-  const perpetual = await runtime.deployL2Contract("Perpetual", [], wallet);
+  const perpetual = await runtime.deployL2Contract("Perpetual", ["ZKnet Perpetual", "1"], wallet);
   logger.info("Perpetual deployed to %s", perpetual.address);
+  await (await perpetual.registerToken(ERC20_ADDRESS, 1)).wait();
+  await runtime.prepareL2Wallet("1100", "1000", "0x8fa6d869bc0453c179b0004518f9043306ef3c43ecadd2387297df5c6a1963a9");
+  await runtime.prepareL2Wallet("1100", "1000", "0x9890a78d684c13a7ce31eb8edc9ba062928a66f9bf4c3e40d1acab2dc32e52d5");
 }
 
 main().catch(error => {
